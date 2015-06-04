eb14f0c813792aeec8243a8a784aa2934e17a4cf
Nuklearwurst <agbrjonas@web.de>
2015-06-04 21:04:44 +0200

    added versioning for the repo modpack.json

--- src/main/java/common/nw/modpack/VersionInfo.java
+++ src/main/java/common/nw/modpack/VersionInfo.java
@@ -1,10 +1,11 @@
 package common.nw.modpack;
 
-@Deprecated
-class VersionInfo {
+public class VersionInfo {
 
-	public static final String INSTALLER_VERSION = "beta";
-	public static final String CREATOR_VERSION = "beta";
-	public static final String UPDATER_VERSION = "beta";
+	/**
+	 * revision of the modpack repo format</br>
+	 * these are only backward compatible
+	 */
+	public static final int REPO_MODPACK_REVISION = 1;
 
 }
